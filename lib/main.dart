a0c0ff4bc590a944c4ffb72bffd67f0634693041
anaskhonprime <smlife202@gmail.com>
2026-07-01 23:06:53 +0900

    Learning FLUTTER default UI WIDGETS

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,6 @@ void main() {
   runApp(const MyApp());
 }
 
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -13,9 +12,36 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(),
-        body: Center(
-          child: Text("Hello"),
+        appBar: AppBar(
+          title: Text("AppBar", style: TextStyle(color: Colors.white)),
+          backgroundColor: Colors.blue,
+        ),
+        body: Align(
+          alignment: Alignment.topLeft,
+          child: Container(
+            margin: EdgeInsets.all(20),
+            padding: EdgeInsets.all(20),
+            decoration: BoxDecoration(
+              border: Border.all(color: Colors.black),
+              borderRadius: BorderRadius.all(Radius.circular(10)),
+              color: Colors.blue,
+            ),
+          
+            width: 200,
+            height: 200,
+          
+            child: Text("Test"),
+          ),
+        ),
+        bottomNavigationBar: BottomAppBar(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Icon(Icons.phone),
+              Icon(Icons.message),
+              Icon(Icons.contact_page),
+            ],
+          ),
         ),
       ),
     );
