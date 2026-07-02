c6d2837637c421e14f895208054a9041ef0e1d58
anaskhonprime <smlife202@gmail.com>
2026-07-02 23:55:47 +0900

    Larning Typography and Layout

--- lib/main.dart
+++ lib/main.dart
@@ -13,24 +13,54 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          title: Text("AppBar", style: TextStyle(color: Colors.white)),
+          title: Text("Seoul", style: TextStyle(color: Colors.white)),
           backgroundColor: Colors.blue,
-        ),
-        body: Align(
-          alignment: Alignment.topLeft,
-          child: Container(
-            margin: EdgeInsets.all(20),
-            padding: EdgeInsets.all(20),
-            decoration: BoxDecoration(
-              border: Border.all(color: Colors.black),
-              borderRadius: BorderRadius.all(Radius.circular(10)),
-              color: Colors.blue,
+          actions: [
+            Container(
+              width: 150,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Icon(Icons.search, color: Colors.white),
+                  Icon(Icons.menu, color: Colors.white),
+                  Icon(Icons.notifications, color: Colors.white),
+                ],
+              ),
             ),
-          
-            width: 200,
-            height: 200,
-          
-            child: Text("Test"),
+          ],
+        ),
+        body: Container(
+          margin: EdgeInsets.all(10),
+          padding: EdgeInsets.all(10),
+          height: 200,
+          decoration: BoxDecoration(
+            border: BoxBorder.all(color: const Color.fromARGB(221, 91, 83, 83)),
+            borderRadius: BorderRadius.circular(5),
+          ),
+          child: Row(
+            children: [
+              Container(child: Image.asset('assets/image1.jpg')),
+              Container(
+                width: 150,
+                margin: EdgeInsets.fromLTRB(10, 0, 0, 0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text("text 1", style: TextStyle(
+                      fontSize: 20, fontWeight: FontWeight.w700)),
+                    Text("text 2"),
+                    Text("text 3"),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Text("Like"),
+                        Icon(Icons.notifications_off_sharp)
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ],
           ),
         ),
         bottomNavigationBar: BottomAppBar(
